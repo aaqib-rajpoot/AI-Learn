fc6a4e8573ff69ec73e612e327cece5f00a70208
Aaaqib RajpooT <73097636+aaqib-rajpoot@users.noreply.github.com>
2024-11-13 14:39:02 +0500

    updated comparison program

--- price_comprasion.py
+++ price_comprasion.py
@@ -1,6 +1,6 @@
 def get_prices():
     products = []
-    store_prices = [] 
+    store_prices = {}
 
 
     #ask for the number of products
@@ -10,6 +10,7 @@ def get_prices():
     #ask for the store names
     num_stores = int(input("enter the number of stores you want to compare (2 or 3):  "))
     store_names = []
+
     for t in range(num_stores):
         store_name = input(f"enter the name of store {t + 1}:")
         store_names.append(store_name)
@@ -17,17 +18,20 @@ def get_prices():
 
     #get the product names and their prices in each store
     for product_index in range(num_products):
-        product_name = (f"\nEnter the name {product_index + 1}:")
+        product_name =input(f"\nEnter product name {product_index + 1}:")
         products.append(product_name)
         product_prices = {}
         for store in store_names:
             price = float(input(f"enter the price of {product_name} at {store}:$"))
             product_prices[store] = price
-        store_prices[product_name] = product_prices
+        store_prices [product_name] = product_prices
+
     return store_prices, store_names, products
+
 def print_comparison(store_prices, store_names, products):
     print("\nprice comprasion summary:\n")
     print(f"{'product':<20}", end=" ")
+
     #print the store names as headers
     for store in store_names:
         print(f"{store:<15}", end =" ")
